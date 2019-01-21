bb52cac6bfd2bccb0f25ae21fb4aed0313600ab7
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2019-01-21 20:17:48 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,11 @@
+public class static void main()
+{int num;
+if (num>0)
+{System.out.println("Number is positive");
+}
+else if(num==0)
+{
+System.out.println("Number is equal to zero");
+}
+else
+System.out.println("Number is negative")
