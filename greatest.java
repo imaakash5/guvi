94b6ee59e63eaf925c08e42095add09ff4b3ad52
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2019-01-28 16:27:30 +0530

    Create greatest.java

---
+++ greatest.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class Aphabet
+{int a;
+public static void main()
+{ Scanner y=new Scanner (System.in);
+a=y.nextInt();
+b=y.nextInt();
+c=y.nextInt();
+if (a>b &&  a>c)
+System.out.println("a is greatest")
+else if (b>a &&  b>c)
+System.out.println("b is greatest")
+else 
+System.out.println("c is greatest")
+}
+}
+
+
+
+
