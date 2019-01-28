1af990c11c19b7c4e039910c2aa9d907f6739b73
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2019-01-28 15:39:59 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Vowel
+{String s;
+public static void main()
+{ Scanner y=new Scanner (System.in);
+s=y.next();
+if (s.equalsIgnoreCase(a)||s.equalsIgnoreCase(e)||s.equalsIgnoreCase(i)||s.equalsIgnoreCase(o)||s.equalsIgnoreCase(u))
+{
+System.out.println("Character is vowel");
+}
+else
+System.out.println("Character is consona");
+}
+}
+
