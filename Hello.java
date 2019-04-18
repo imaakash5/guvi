38823e7bd908e2f558c6110b6d4312053b788280
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2019-04-18 11:44:16 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Input
+{
+	public static void main(String[] args) {
+	    int n;
+		Scanner y=new Scanner(System.in);
+		System.out.println("input n");
+		n=y.nextInt();
+	
+	    for(int i=1;i<=n;i++)
+	    {
+	        System.out.println("Hello");
+	    }
+	}
+}
