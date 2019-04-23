b35636a6cf7392810914378db8d91a84f2df5a42
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2019-04-23 09:28:24 +0530

    Create Exponent.java

---
+++ Exponent.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ class Exponent
+{
+public static void main(String[] args)
+
+{int n,k;
+double result;
+    Scanner y=new Scanner (System.in);
+System.out.println("Enter n ");
+n=y.nextInt();
+System.out.println("Enter k ");
+k=y.nextInt();
+result=Math.pow(n,k);
+System.out.println(result);}
+}
